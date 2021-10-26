b684f6af9f64868403305d9af99b678803251d46
dennetwebdesigner <dennetwebdesigner@gmail.com>
2021-10-26 18:02:48 -0300

    class app para conexao ao server e cors adicionado

---
+++ api/src/app.js
@@ -0,0 +1,25 @@
+class App {
+    constructor() {
+        //Requisição Express
+        this.express = require('express');
+        const app = this.express();
+
+        //cors permissões
+        this.cors = require('cors')
+
+        // criei um server
+        this.server = require('http').createServer(app);
+    }
+
+    middleware() {
+        this.app.use(this.express.json())
+        this.app.use(this.cors())
+    }
+
+    routes() {
+        //rotas public -> front-end
+        this.app.use('/', this.express.static(require('path').resolve(__dirname, 'public')));
+    }
+}
+
+module.exports = new App().server
--- api/src/index.js
+++ api/src/index.js
@@ -1,22 +1,7 @@
-//Requisição Express
-const express = require('express');
-// requisições 
-const app = express();
-
-// criei um server
-const server = require('http').createServer(app);
-
-//rotas public -> front-end
-app.use('/', express.static(require('path').resolve(__dirname, 'public')));
-
-//Rota Principal
-app.get('/api/', (req, res) => {
-    res.render("../public/");
-});
-
+const app = require('./app')
 
 //Servidor principal
-server.listen(3000, () => {
+app.listen(3000, () => {
     console.log("Servidor Rodando na Porta 3000");
     console.log("Para encerrar 'CTRL + C'")
 })
